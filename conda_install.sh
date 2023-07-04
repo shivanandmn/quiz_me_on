607cc5c8aaf5522ba9dd7a4b2338c805dd4613a5
shivanand <shivanandnaduvin@gmail.com>
2023-07-04 05:59:32 +0530

    updated conda isntall

---
+++ conda_install.sh
@@ -0,0 +1,2 @@
+bash https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh
+source ~/miniforge3/bin/activate
--- mongo_setup.sh
+++ mongo_setup.sh
@@ -1,4 +1,5 @@
 ## install in wsl:https://dev.to/arielro85/install-mongodb-on-wsl2-and-ubuntu2204-33j4
+## https://www.mongodb.com/developer/products/mongodb/mongodb-on-raspberry-pi/
 sudo apt-get install gnupg
 curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
@@ -10,4 +11,14 @@ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gp
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 mkdir -p data/db
-mongod --dbpath data/db
+# mongod --dbpath data/db
+# Ensure mongod config is picked up:
+sudo systemctl daemon-reload
+
+# Tell systemd to run mongod on reboot:
+sudo systemctl enable mongod
+
+# Start up mongod!
+sudo systemctl start mongod
+
+# $ sudo systemctl status mongod
